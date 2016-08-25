4e2bd85aabb86ad3e8b4bfe5a0615f8167dc6211
soooner <705442809@qq.com>
2016-08-25 14:11:54 +0800

    no message

---
+++ Submodule/Submodule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Submodule
+//
+//  Created by jinlong sheng on 16/8/25.
+//  Copyright © 2016年 sjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Submodule/Submodule/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Submodule
+//
+//  Created by jinlong sheng on 16/8/25.
+//  Copyright © 2016年 sjl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
